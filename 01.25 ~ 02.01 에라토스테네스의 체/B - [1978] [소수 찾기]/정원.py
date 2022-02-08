7a2b14c51a01ecb8274cc82ccaf06bfda100af78
sorrow4468 <sorrow4468@gmail.com>
2022-02-08 19:11:52 +0900

    소수의 연속합 코드 업로드 | A B C 문제 주석 추가

--- 01.25 ~ 02.01 에라토스테네스의 체/B - [1978] [소수 찾기]/정원.py
+++ 01.25 ~ 02.01 에라토스테네스의 체/B - [1978] [소수 찾기]/정원.py
@@ -1,26 +1,29 @@
-dp = [False, False] + [True] * (999)
-primes = []
+# 문제에서 제공된 범위 N은 1000이하의 자연수
+dp = [False, False] + [True] * (999) # 1000까지의 소수
+primes = [] # 소수를 담을 리스트
 
-for i in range(2, 1001):
-    if dp[i]:
-        primes.append(i)
+for i in range(2, 1001): # 2부터 1000까지
+    if dp[i]: # 소수다??
+        primes.append(i) # 소수 리스트에 추가
         for j in range(i*2, 1001, i):
-            dp[j] = False
+            # i의 배수는 전부 소수가 아니다
+            dp[j] = False # 네
 
 N = int(input())
 nums = list(map(int, input().split()))
 
-maxx = nums[-1]
-result = 0
+maxx = nums[-1] # 입력값들 중 가장 큰 값 : 맨 뒤 값
+result = 0 # 출력할 소수의 개수
 
-for p in primes:
-    for n in nums:
-        if n == p:
-            if n == 1:
-                continue
-            result += 1
+for p in primes: # 1000까지의 소수들 중
+    for n in nums: # 입력받은 숫자들 중
+        if n == p: # 같다면 : 소수라면
+            if n == 1: # 근데 1은 소수가 아님
+                continue # 패스
+            result += 1 # 소수 개수 +1
     
-    if p == maxx:
-        break
+    if p >= maxx: # 1000까지 구한 소수 전부 안돌고
+        break # 마지막 입력값보다 현재 보는 소수가 크거나 같으면 
+        # 종료
 
-print(result)
+print(result) # 소수 개수 출력
--- 01.25 ~ 02.01 에라토스테네스의 체/C - [1929] [소수 구하기]/정원.py
+++ 01.25 ~ 02.01 에라토스테네스의 체/C - [1929] [소수 구하기]/정원.py
@@ -1,16 +1,18 @@
 M, N = map(int, input().split())
 
+# 소수인지 확인할 DP배열
 dp = [False, False] + [True] * (N-1)
 
-primes = []
+primes = [] # 소수를 구해서 담을 리스트
 
 for i in range(2, N+1):
-    if dp[i]:
-        primes.append(i)
-        for j in range(i*2, N+1, i):
-            if dp[j]:
+    if dp[i]: # 2부터 시작, 소수를 찾았다??
+        primes.append(i) # 소수 리스트에 추가
+        for j in range(i*2, N+1, i): 
+            # i의 배수는 전부 소수가 아님
+            if dp[j]: # 최적화
                 dp[j] = False
 
-for p in primes:
-    if p >= M:
-        print(p)
+for p in primes: # N까지 구한 소수들 중에서
+    if p >= M: # M보다 큰 소수다?
+        print(p) # 출력
