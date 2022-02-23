b21e565cc1d57569504700196e754d737daeb687
정원 <82122491+sorrow4468@users.noreply.github.com>
2022-02-24 08:52:53 +0900

    정원 C - [1969] [DNA] 주석 추가

--- 02.21 ~ 03.01 구현/C - [1969] [DNA]/정원.py
+++ 02.21 ~ 03.01 구현/C - [1969] [DNA]/정원.py
@@ -1,8 +1,10 @@
-N, M = map(int, input().split())
+N, M = map(int, input().split()) # DNA의 개수 N과 길이 M
 
-DNAs = [input() for _ in range(N)]
+DNAs = [input() for _ in range(N)] # DNA들을 담은 리스트
 
-dictt = {
+dictt = { # DNA는 ACGT 네가지로 구성되며, 각각의 인덱스를 지정
+    # 딕셔너리를 숫자<->문자 양방향으로 만들지 않고
+    # 한 쪽은 키값, 다른 한 쪽은 밸류값 썼으면 됐는데 귀찮았습니다 ㅈㅅ...
     'A': 0,
     'C': 1,
     'G': 2,
@@ -13,21 +15,32 @@ dictt = {
     3: 'T',
 }
 
-HD = 0
+HD = 0 # Hamming Distance, 구하고자 하는 값
 
-for i in range(M):
+for i in range(M): # DNA의 길이만큼
 
-    table = [0] * 4
+    table = [0] * 4 # ACGT
 
-    for d in DNAs:
+    # 모든 DNA의 i번째 글자에 대하여, 가장 많이 등장한 DNA값이
+    # 가장 최소의 Hamming Distance값을 가지게 됨
+    for d in DNAs: # 각각의 DNA의 글자 마다
         table[dictt[d[i]]] += 1
+        # d[i] = 각 DNA의 i번째 글자
+        # dictt[d[i]] = d[i]를 ACGT에 맞게 인덱스로 변환한 값
+        # table[dictt[d[i]]] = table에서 d[i]를 변환한 인덱스값에 해당하는 칸
+        # ex) d = TATGATAC, i = 0 일 때, d[i] = 'T'
+        # dictt[d[i]] = dictt['T'] = 3
+        # table[dictt[d[i]]] = table[3]
+        # table[3] += 1 이 실행됨
         
-    for j in range(4):
-        if j != table.index(max(table)):
-            HD += table[j]
+    # 최소 Hamming Distance값을 구하자
+    for j in range(4): # 테이블 순회
+        if j != table.index(max(table)): # Hamming Distance가 아닌 DNA일때
+            HD += table[j] # 그 DNA가 등장한 횟수를 Hamming Distance에 더하기
 
-    print(dictt[table.index(max(table))], end='')
+    print(dictt[table.index(max(table))], end='') 
+    # 매 i에 대하여 Hamming Distance가 되는 DNA글자를 구하였고 이를 출력
 
-print()
+print() # 줄바꿈
 
-print(HD)
+print(HD) # 최소 Hamming Distance값
