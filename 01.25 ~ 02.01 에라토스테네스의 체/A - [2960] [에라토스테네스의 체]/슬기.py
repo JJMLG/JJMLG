0572612a24c139a4ae52a4f3edb37009f2a46817
문슬기 <tmfrlghtjr@gmail.com>
2022-02-26 22:43:41 +0900

    merge seulgi

--- 01.25 ~ 02.01 에라토스테네스의 체/A - [2960] [에라토스테네스의 체]/슬기.py
+++ 01.25 ~ 02.01 에라토스테네스의 체/A - [2960] [에라토스테네스의 체]/슬기.py
@@ -10,7 +10,7 @@ cnt = 0
 for i in range(2, n+1):
     if ls[i]:
         primes.append(i)
-        for j in range(2*i, n+1, i):
+        for j in range(i, n+1, i):
             ls[j] = False
             cnt += 1
             if cnt == k:
