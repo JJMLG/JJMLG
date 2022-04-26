0f5bf2a977fc128b0610863a0318fa89f6f513ae
문슬기 <tmfrlghtjr@gmail.com>
2022-04-26 12:11:55 +0900

    seulgi upload

--- 04.19 ~ 04.26 구현 및 DFS&BFS/D - 2583 영역 구하기/슬기.py
+++ 04.19 ~ 04.26 구현 및 DFS&BFS/D - 2583 영역 구하기/슬기.py
@@ -1,19 +1,45 @@
 import sys
 sys.stdin = open('input.txt')
 
+
 m, n, k = map(int, input().split())
 # print(m, n , k)
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
-paper = [[0] * (n+1) for _ in range(m+1)]
-print(paper)
+square = []
 
+paper = [[0] * n for _ in range(m)]
+# print(paper)
+
+# 네모 채우기
 for _ in range(k):
     x1, y1, x2, y2 = map(int, input().split())
-    # print(x1, y1, x2, y2)
-    for i in range(x2-x1+1):
-        if paper[i][y1] == 0:
-            paper[i][y1] += 1
-        # for j in range(y2-y1+1):
-        #     if paper[i][j] == 0:
-        #         paper[i][j] += 1
-print(paper)
+
+    for i in range(y1, y2):
+        # print(i)
+        for j in range(x1, x2):
+            paper[i][j] = 1
+# print(paper)
+
+for u in range(m):
+    for l in range(n):
+        if paper[u][l] == 0:
+            count = 1
+            paper[u][l] = 1
+            queue = [[u, l]]
+            while queue:
+                x, y = queue[0][0], queue[0][1]
+                del queue[0]
+                for a in range(4):
+                    x1 = x + dx[a]
+                    y1 = y + dy[a]
+                    if 0 <= x1 < m and 0 <= y1 < n and paper[x1][y1] == 0:
+                        paper[x1][y1] = 1
+                        count += 1
+                        queue.append([x1, y1])
+            square.append(count)
+print(len(square))
+square.sort()
+for z in square:
+    print(z, end=' ')
