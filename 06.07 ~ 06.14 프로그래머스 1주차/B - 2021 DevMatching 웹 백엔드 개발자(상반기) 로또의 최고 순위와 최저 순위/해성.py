48fcb54e69790f4b143d0704671e4afa5a5a9241
tg8685 <tg8685@gmail.com>
2022-06-14 19:49:02 +0900

    Docs: update B - 2021 DevMatching 웹 백엔드 개발자(상반기) 로또의 최고 순위와 최저 순위

--- 06.07 ~ 06.14 프로그래머스 1주차/B - 2021 DevMatching 웹 백엔드 개발자(상반기) 로또의 최고 순위와 최저 순위/해성.py
+++ 06.07 ~ 06.14 프로그래머스 1주차/B - 2021 DevMatching 웹 백엔드 개발자(상반기) 로또의 최고 순위와 최저 순위/해성.py
@@ -1,15 +1,22 @@
 def solution(lottos, win_nums):
     lottos = sorted(lottos)
     win_nums = sorted(win_nums)
+    print(lottos, win_nums)
+    #     rank는 일차하는 숫자 인덱스에 값이 순위
     rank = [6, 6, 5, 4, 3, 2, 1]
+
+    # 일치하는 갯수 cnt
     cnt = 0
+    # 0의 갯수
     zero_cnt = 0
     for i in lottos:
+        # 만약에 0이 아니고 당첨 숫자에 있다면
         if i != 0 and i in win_nums:
             cnt += 1
+        # 만약에 0이면 0 갯수
         if i == 0:
             zero_cnt += 1
-
+    # 최고 당첨 순위, 최저 당첨 순위
     answer = [rank[cnt + zero_cnt], rank[cnt]]
 
     return answer
