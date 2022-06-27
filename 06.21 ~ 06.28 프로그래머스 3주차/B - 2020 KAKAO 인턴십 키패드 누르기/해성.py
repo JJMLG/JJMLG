253949202d3c0d3ac411e5b2176c3cbdac07452a
tg8685 <tg8685@gmail.com>
2022-06-27 13:33:12 +0900

    Docs: add B - 2020 KAKAO 인턴십 키패드 누르 - 해성

---
+++ 06.21 ~ 06.28 프로그래머스 3주차/B - 2020 KAKAO 인턴십 키패드 누르기/해성.py
@@ -0,0 +1,38 @@
+def solution(numbers, hand):
+    answer = ''
+    keyPad = [[1,2,3], [4,5,6], [7,8,9], ['*', 0,'#']]
+    posNum = dict()
+    leftThumb = [3,0]
+    rightThumb = [3,2]
+    for number in numbers:
+        for h in range(4):
+            for c in range(3):
+                if keyPad[h][c]==number:
+                    posNum[number]=[h, c]
+            #     왼쪽사이드면
+                    if c ==0 and h<3:
+                        answer+='L'
+                        leftThumb = [h,c]
+                    elif c== 2 and h<3:
+                        answer+='R'
+                        rightThumb = [h,c]
+                    elif c==1:
+            #             같을때
+                        if abs(leftThumb[0]- h) + abs(leftThumb[1] - c) == abs(rightThumb[0]- h) + abs(rightThumb[1] - c):
+                            if hand=="right":
+                                answer+='R'
+                                rightThumb = [h,c]
+                            else:
+                                answer+='L'
+                                leftThumb = [h,c]
+                        elif abs(leftThumb[0]- h) + abs(leftThumb[1] - c) < abs(rightThumb[0]- h) + abs(rightThumb[1] - c):
+                            answer+='L'
+                            leftThumb = [h,c]
+                        else:
+                            answer+='R'
+                            rightThumb = [h,c]
+    return answer
+
+numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
+hand = 'right'
+solution(numbers, hand)
