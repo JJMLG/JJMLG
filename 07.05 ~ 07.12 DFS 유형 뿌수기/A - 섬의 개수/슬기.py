ab440e3ee7f992b961af854d6082398b46f8abe4
문슬기 <tmfrlghtjr@gmail.com>
2022-07-11 00:59:42 +0900

    upload seulgi

---
+++ 07.05 ~ 07.12 DFS 유형 뿌수기/A - 섬의 개수/슬기.py
@@ -0,0 +1,46 @@
+import sys
+sys.setrecursionlimit(99999)
+sys.stdin = open('input.txt')
+# from collections import deque
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# 대각선 조건 넣어야 됨
+def dfs(x, y):
+    visited[x][y] = 1
+    stack.append([x, y])
+    # print(stack)
+    for k in range(4):
+        nx = x + dx[k]
+        ny = y + dy[k]
+
+        if 0 <= nx < h and 0 <= ny < w and land[nx][ny] == 1 and visited[nx][ny] == 0:
+            land[nx][ny] = 0
+            dfs(nx, ny)
+            # pass
+
+    return cnt
+
+
+
+while True:
+    w, h = map(int, input().split())
+    # input 종료 조건
+    cnt = 0
+    if w == 0 and h == 0:
+        break
+    visited = [[0] * w for _ in range(h)]
+    stack = []
+    # print(visited)
+
+    land = [list(map(int, input().split())) for _ in range(h)]
+    # print(land)
+
+    for i in range(len(land)):
+        for j in range(len(land[i])):
+            if land[i][j] == 1:
+                cnt += 1
+                dfs(i, j)
+
+    print(cnt)
--- 07.05 ~ 07.12 DFS 유형 뿌수기/B - 단지번호붙이기/슬기.py
+++ 07.05 ~ 07.12 DFS 유형 뿌수기/B - 단지번호붙이기/슬기.py
@@ -1,50 +1,6 @@
 import sys
 sys.setrecursionlimit(99999)
 sys.stdin = open('input.txt')
-from collections import deque
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(x, y):
-    cnt = 0
-    queue = deque()
-    queue.append([x, y])
-
-    while queue:
-        x, y = queue.popleft()
-        apartment[x][y] = 0
-
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-
-            if 0 <= nx < T and 0 <= ny < T and apartment[nx][ny] == 1 and visited[nx][ny] == 0:
-                queue.append([nx, ny])
-                visited[nx][ny] = 1
-                cnt += 1
-
-    return cnt
 
 
-T = int(input())
-apartment = [list(map(int, input())) for _ in range(T)]
-
-visited = [[0] * T for _ in range(T)]
-# print(visited)
-# print(apartment)
-house = 0
-order = []
-
-for i in range(T):
-    for j in range(T):
-        if apartment[i][j] == 1:
-            result = bfs(i, j)
-            house += 1
-            order.append(result + 1)
-
-print(house)
-order.sort()
-
-for l in order:
-    print(l)
+# dfs로 다시 풀기
