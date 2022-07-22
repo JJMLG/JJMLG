cc9ccf19e6902e0b5d5285090ab5119eead0a95c
문슬기 <tmfrlghtjr@gmail.com>
2022-07-22 18:33:36 +0900

    seulgi 11478 upload

---
+++ 07.19 ~ 07.31 집합과 해시맵/B - 11478 서로 다른 부분 문자열의 개수/슬기.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open('input.txt')
+
+
+s = input()
+ans = set()
+#
+# for i in range(len(s)):
+#     for j in range(i, len(s)):
+#         ans.add(s[i:j+1])
+# print(len(ans))
+
+
+idx = 0
+flag = 1
+
+while flag + idx <= len(s):
+    ans.add(s[idx:idx + flag])
+    if idx + flag >= len(s):
+        flag += 1
+        idx = 0
+    else:
+        idx += 1
+print(len(ans))
