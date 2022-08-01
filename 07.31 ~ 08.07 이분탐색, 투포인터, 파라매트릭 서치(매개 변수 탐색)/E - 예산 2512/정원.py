8df7df8a6fc5d41c160cfba5fe5e590cf94b17dd
정원 <sorrow4468@gmail.com>
2022-08-02 03:38:57 +0900

    정원 E - 예산 2512 주석 추가

--- 07.31 ~ 08.07 이분탐색, 투포인터, 파라매트릭 서치(매개 변수 탐색)/E - 예산 2512/정원.py
+++ 07.31 ~ 08.07 이분탐색, 투포인터, 파라매트릭 서치(매개 변수 탐색)/E - 예산 2512/정원.py
@@ -1,20 +1,21 @@
 def binary(start, end):
     global result, M
-    if start > end: return
-    budget = (start + end) // 2
-    tmp = 0
+    if start > end: return # 탐색 범위를 벗어남
+    budget = (start + end) // 2 # 이번에 탐색해볼 예산
+    tmp = 0 # 해당 예산으로 도시들마다 예산 지급시 총 금액
     for b in B: tmp += min(b, budget)
-    if tmp <= M:
-        if budget > result: 
-            result = budget
-        binary(budget+1, end)
-    else:
-        binary(start, budget-1)
+    if tmp <= M: # 예산을 다 지급했는데 아직 여유가 있다면
+        if budget > result: # 그 예산이 기존 최대 예산보다 크다면
+            result = budget # 최대값 갱신
+        binary(budget+1, end) # 다음 큰 범위 탐색
+    else: # 해당 예산으로 지급 시, 총 예산을 초과한다면
+        binary(start, budget-1) # 다음 작은 범위 탐색
 
 N = int(input())
 B = sorted(list(map(int, input().split()))) # budget
-M = int(input())
-S, E = 0, M
-result = 0
-binary(S, E)
-print(min(B[-1], result))
+M = int(input()) # 총 예산
+S, E = 0, M # 이분탐색은 매우 효율적인 알고리즘이므로 범위 시작과 끝을 무지성으로 지정해도 잘 돌아간다
+result = 0 # 결과값 초기화
+binary(S, E) # 이론상 최대 예산을 구하기
+# 모든 도시의 예산의 합이 전체 예산보다 클 경우 가장 큰 예산을 출력
+print(min(B[-1], result)) 
