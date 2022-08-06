a68423291b6a762e708d61758bc7e3f66caab0b2
문슬기 <tmfrlghtjr@gmail.com>
2022-08-07 00:51:16 +0900

    seulgi upload

--- 07.31 ~ 08.07 이분탐색, 투포인터, 파라매트릭 서치(매개 변수 탐색)/D - 숫자 카드 2 10816/슬기.py
+++ 07.31 ~ 08.07 이분탐색, 투포인터, 파라매트릭 서치(매개 변수 탐색)/D - 숫자 카드 2 10816/슬기.py
@@ -6,39 +6,28 @@ card = sorted(list(map(int, input().split())))
 m = int(input())
 find_card = list(map(int, input().split()))
 
-def binary(k, arr, left, right):
+def binary(find, arr, left, right):
     if left > right:
         return 0
 
     mid = (left + right) // 2
 
-    if k == arr[mid]:
-        a, b = 1, 1
-        while mid - a >= left:
-            if arr[mid-a] != arr[mid]:
-                break
-            else:
-                a += 1
-
-        while mid + b <= right:
-            if arr[mid+b] != arr[mid]:
-                break
-            else:
-                b += 1
-        return a + b -1
-
-    elif k < arr[mid]:
-        return binary(k, arr, left, mid-1)
+    if arr[mid] == find:
+        return arr[left:right+1].count(find)
+    elif arr[mid] < find:
+        return binary(find, arr, mid+1, right)
     else:
-        return binary(k, arr, mid+1, right)
-
-
-
+        return binary(find, arr, left, mid-1)
 
 temp = {}
 for i in card:
-    left = 0
-    right = len(card) - 1
     if i not in temp:
-        temp[i] = binary(i, card, left, right)
-print(' '.join(str(temp[x]) if x in temp else '0' for x in find_card))
+        temp[i] = binary(i, card, 0, n-1)
+
+print(' '.join(str(temp[x]) if x in temp else '0' for x in find_card ))
+
+# for j in find_card:
+#     if j in temp:
+#         print(' '.join(str(temp[j])), end=' ')
+#     else:
+#         print('0', end=' ')
