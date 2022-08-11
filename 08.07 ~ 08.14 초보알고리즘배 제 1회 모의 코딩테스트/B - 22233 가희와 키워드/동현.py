1652aab46448ebb547954b098a297b170e8d1a66
ehdgus <rhkrehd619@naver.com>
2022-08-11 11:49:09 +0900

    sol

---
+++ 08.07 ~ 08.14 초보알고리즘배 제 1회 모의 코딩테스트/B - 22233 가희와 키워드/동현.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+dict = {}
+n,m = map(int,input().split())
+for i in range(n):
+    dict[input().rstrip()] = 1
+
+tot = len(dict)
+cnt = 0
+for j in range(m):
+    ls = input().rstrip().split(',')
+    for k in ls:
+        if k not in dict:
+            continue
+        if dict[k] == 1:
+            dict[k] = 0
+            cnt += 1
+    
+    print(tot-cnt)
+    
+    
