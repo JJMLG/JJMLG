750ba7c6ca2f68fa20e0fd31f963cf944b5544ce
문슬기 <tmfrlghtjr@gmail.com>
2022-08-14 20:00:26 +0900

    seulgi upload

--- 08.07 ~ 08.14 초보알고리즘배 제 1회 모의 코딩테스트/C - 5525 IOIOI/슬기.py
+++ 08.07 ~ 08.14 초보알고리즘배 제 1회 모의 코딩테스트/C - 5525 IOIOI/슬기.py
@@ -1,2 +1,23 @@
 import sys
 sys.stdin = open('input.txt')
+
+n = int(input())
+m = int(input())
+s = input()
+# s = list(input())
+# print(s)
+
+p = 'I'
+for _ in range(n):
+    # p.append('O')
+    # p.append('I')
+    p += 'OI'
+# print(p)
+
+cnt = 0
+for i in range(m):
+    if s[i] == 'I':
+        # print(s[i:len(p)+i])
+        if s[i:len(p)+i] == p:
+            cnt += 1
+print(cnt)
