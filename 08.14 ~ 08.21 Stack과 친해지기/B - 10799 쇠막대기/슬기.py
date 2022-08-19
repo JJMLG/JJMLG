635ad49f6c48e21139990bca9e5780630fc9ae98
문슬기 <tmfrlghtjr@gmail.com>
2022-08-20 00:03:03 +0900

    seulgi upload

--- 08.14 ~ 08.21 Stack과 친해지기/B - 10799 쇠막대기/슬기.py
+++ 08.14 ~ 08.21 Stack과 친해지기/B - 10799 쇠막대기/슬기.py
@@ -2,20 +2,4 @@ import sys
 sys.stdin = open('input.txt')
 
 stick = input()
-# print(stick)
 stack = []
-
-result = 0
-for i in range(len(stick)):
-    if stick[i] == '(':
-        stack.append('(')
-    else:
-        if stick[i-1] == '(':
-            stack.pop()
-            result += len(stack)
-
-        else:
-            stack.pop()
-            result += 1
-
-print(result)
