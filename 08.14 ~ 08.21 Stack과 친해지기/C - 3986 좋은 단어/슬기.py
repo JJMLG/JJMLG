b9a1bda9b0680b5067a0f8e5fe41f6bf0f47b789
문슬기 <tmfrlghtjr@gmail.com>
2022-08-19 21:04:15 +0900

    upload seulgi

--- 08.14 ~ 08.21 Stack과 친해지기/C - 3986 좋은 단어/슬기.py
+++ 08.14 ~ 08.21 Stack과 친해지기/C - 3986 좋은 단어/슬기.py
@@ -8,4 +8,17 @@ for _ in range(n):
     word = input()
     stack = []
 
+    for i in range(len(word)):
+        if not stack:
+            stack.append(word[i])
+        else:
+            if word[i] == stack[-1]:
+                stack.pop()
+                # cnt += 1
+            else:
+                stack.append(word[i])
+    # print(stack)
+    if not stack:
+        cnt += 1
 
+print(cnt)
