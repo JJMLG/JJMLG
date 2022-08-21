5039f2be1d72e8e21679ddeac6cc9d7a2408689f
문슬기 <tmfrlghtjr@gmail.com>
2022-08-21 16:01:51 +0900

    seulgi upload

--- 08.14 ~ 08.21 Stack과 친해지기/D - 2841 외계인의 기타 연주/슬기.py
+++ 08.14 ~ 08.21 Stack과 친해지기/D - 2841 외계인의 기타 연주/슬기.py
@@ -2,24 +2,3 @@ import sys
 sys.stdin = open('input.txt')
 
 n, p = map(int, input().split())
-
-stack = [[] for _ in range(7)] # 1~6번 줄 까지 있음
-cnt = 0
-for i in range(n):
-    mel_n, mel_p = map(int, input().split())
-
-    if not stack[mel_n-1]:
-        stack[mel_n-1].append(mel_p)
-        cnt += 1
-        # print(stack)
-    else:
-        while stack[mel_n-1] and mel_p < stack[mel_n-1][-1]:
-            stack[mel_n-1].pop()
-            cnt += 1
-        if not stack[mel_n-1] or mel_p > stack[mel_n-1][-1]:
-            stack[mel_n-1].append(mel_p)
-            cnt += 1
-        else:
-            pass
-
-print(cnt)
--- 08.14 ~ 08.21 Stack과 친해지기/F - 2493 탑/슬기.py
+++ 08.14 ~ 08.21 Stack과 친해지기/F - 2493 탑/슬기.py
@@ -1,2 +1,28 @@
 import sys
 sys.stdin = open('input.txt')
+
+n = int(input())
+top = list(input().split())
+# top = sorted(list(input().split()))
+# print(top)
+stack = []
+result = [0] * n
+top_idx = 0
+
+for i in range(n):
+    if not stack:
+        stack.append(top[i])
+    else:
+        if stack[-1] < top[i]:
+            stack.append(top[i])
+            result[i] = top_idx
+            top_idx = i + 1
+            # print(top_idx)
+        elif stack[-1] >= top[i]:
+            result[i] = top_idx
+            stack.append(top[i])
+            print(stack)
+if max(result) == 0:
+    print(0)
+else:
+    print(*result)
