320931e4999e8944eb85ccb57a3c571685e069cf
정원 <sorrow4468@gmail.com>
2022-08-28 15:33:21 +0900

    정원 A - 1753 최단경로

---
+++ 08.21 ~ 08.28 최단 거리 알고리즘 - 다익스트라(Dijkstra)/A - 1753 최단경로/정원.py
@@ -0,0 +1,28 @@
+import sys
+import heapq # 탐색 시 가장 작은 값을 탐색하기 위함
+
+input = sys.stdin.readline
+INF = int(1e9) # 가상의 최대값
+
+def dijkstra(start):
+    distance[start] = 0 # 시작점에서 시작점으로 가는 최단 경로값은 0
+    heapq.heappush(Q, (0, start))
+    while Q:
+        dist, now = heapq.heappop(Q) # 가장 작은 값을 heappop으로 꺼내기
+        for next, weight in data[now]: # 현재 노드에서 갈 수 있는 노드와 가중치들을 순회하면서
+            cost = dist + weight # 경로값은 들고 있는 경로값 + 가중치
+            if cost < distance[next]: # 더 작은 경로값이 발견되면
+                distance[next] = cost # 최소 경로값 갱신
+                heapq.heappush(Q, (cost, next)) # 해당 경로와 다음 노드를 heappush로 가장 앞에 추가
+
+V,E = map(int, input().split())
+start = int(input())
+data = [[] for _ in range(V+1)]
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    data[u].append((v, w)) # 노드 u에서 갈 수 있는 노드v와 그 때의 가중치 w
+distance = [INF]*(V+1) # 가상의 최대 경로값 배열
+Q = [] # heapq의 메소드만 사용함, deque와 다르게, 일반적인 리스트 선언
+dijkstra(start) # start부터 출발
+for d in distance[1:]: # 0번은 인덱스 맞추기용
+    print(d if d != INF else 'INF') # 값이 있으면 해당 경로값, 없으면 'INF'
