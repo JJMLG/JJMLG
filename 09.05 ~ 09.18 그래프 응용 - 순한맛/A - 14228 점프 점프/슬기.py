3f97e437c6666015e4b3365aa16bff210cf31227
문슬기 <tmfrlghtjr@gmail.com>
2022-09-06 23:23:38 +0900

    seulgi upload

--- 09.05 ~ 09.18 그래프 응용 - 순한맛/A - 14228 점프 점프/슬기.py
+++ 09.05 ~ 09.18 그래프 응용 - 순한맛/A - 14228 점프 점프/슬기.py
@@ -1,2 +1,24 @@
 import sys
 sys.stdin = open('input.txt')
+
+stone = int(input())
+distance = list(map(int, input().split()))
+start = int(input())
+
+visited = [0] + [0] * stone
+
+
+def dfs(graph, s):
+    jump = graph[s]
+
+    if visited[s+1] == 0:
+        visited[s+1] = 1
+
+    
+
+
+
+# for i in range(1, len(distance)+1):
+#     if visited[start] == 0:
+#         visited[start] = 1
+#         dfs(distance, start)
