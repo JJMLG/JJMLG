68d89982f60dea66fbd647e14e9d5e5dd2c145fe
문슬기 <tmfrlghtjr@gmail.com>
2022-09-10 23:03:37 +0900

    upload seulgi

--- 09.05 ~ 09.18 그래프 응용 - 순한맛/A - 14228 점프 점프/슬기.py
+++ 09.05 ~ 09.18 그래프 응용 - 순한맛/A - 14228 점프 점프/슬기.py
@@ -8,3 +8,17 @@ start = int(input())
 visited = [0] * stone
 
 cnt = 1
+
+def dfs(x):
+    visited[x] = 1
+    left = x - distance[x]
+    right = x + distance[x]
+
+    if left >= 0 and visited[left] == 0:
+        dfs(left)
+    if right < stone and visited[right] == 0:
+        dfs(right)
+
+
+dfs(start-1)
+print(sum(visited))
