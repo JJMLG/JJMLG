9f5740a32c3c5b38aaabf49944c7dfbe162df162
문슬기 <tmfrlghtjr@gmail.com>
2022-09-12 23:12:23 +0900

    upload

--- 09.05 ~ 09.18 그래프 응용 - 순한맛/E - 18243 Small World Network/슬기.py
+++ 09.05 ~ 09.18 그래프 응용 - 순한맛/E - 18243 Small World Network/슬기.py
@@ -1,2 +1,13 @@
 import sys
 sys.stdin = open('input.txt')
+
+n, k = map(int, input().split())
+
+graph = [[] for _ in range(k+1)]
+
+for _ in range(k):
+    a, b = map(int, input().split())
+
+    graph[a].append(b)
+    # graph[b].append(a)
+    print(graph)
