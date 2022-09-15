41ddbee42133236f3e659f5f4a880aa0ca0b3e02
문슬기 <tmfrlghtjr@gmail.com>
2022-09-15 21:15:16 +0900

    upload

--- 09.05 ~ 09.18 그래프 응용 - 순한맛/B - 10472 십자뒤집기/슬기.py
+++ 09.05 ~ 09.18 그래프 응용 - 순한맛/B - 10472 십자뒤집기/슬기.py
@@ -1,56 +1,60 @@
+import copy
 import sys
 sys.stdin = open('input.txt')
-from collections import deque
+
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+space = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
 
-def bfs(x, y, arr):
-    global cnt, visited
-    final_board = [[0] * 3 for _ in range(3)]
-    q = deque()
-    q.append(final_board)
-    visited[x][y] = 1
-    # print(q)
-    while q:
-        l = len(q)
+def bfs(x, y, tmp):
+    if tmp[x][y] == '*':
+        tmp[x][y] = '.'
+    elif tmp[x][y] == '.':
+        tmp[x][y] = '*'
 
-        for _ in range(l):
-            board = q.popleft()
-            if board == final_board:
-                return cnt
+    for k in range(4):
+        nx = x + dx[k]
+        ny = y + dy[k]
 
-    # if visited[x][y] == 1:
-    #     visited[x][y] = 0
-    #
-    # for z in range(4):
-    #     nx = x + dx[z]
-    #     ny = y + dy[z]
-    #
-    #     if 0 <= nx < 3 and 0 <= ny < 3 and visited[nx][ny] == 1:
-    #         visited[nx][ny] = 0
-    #         bfs(nx, ny)
-    #         print(visited)
+        if nx < 0 or nx >= 3 or ny < 0 or ny >= 3:
+            continue
+
+        if tmp[nx][ny] == '*':
+            tmp[nx][ny] = '.'
+        elif tmp[nx][ny] == '.':
+            tmp[nx][ny] = '*'
+
+    return tmp
+
+
+def solve(board, cnt, idx):
+    global ans, arr, space
+
+    if board == arr:
+        ans = min(ans, cnt)
+
+    if idx > 8:
+        return
+
+    x = space[idx][0]
+    y = space[idx][1]
+
+    paint_board = copy.deepcopy(board)
+    paint_board = bfs(paint_board, x, y)
+
+    solve(paint_board, cnt+1, idx+1)
+    solve(board, cnt, idx+1)
+
 
 
 p = int(input())
 
 for _ in range(p):
-    board = list(input() for _ in range(3))
-    # print(board)
-    change_board = [[0] * 3 for _ in range(3)]
-    visited = [[0] * 3 for _ in range(3)]
-    # print(visited)
-    cnt = 0
+    arr = [list(input() for _ in range(3))]
+    init = [['.', '.', '.'] for _ in range(3)]
 
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] == '*':
-                change_board[i][j] = 1
+    ans = 1000000000
 
-    for i in range(3):
-        for j in range(3):
-            if change_board[i][j] == 1:
-                # visited[i][j] = 0
-                bfs(i, j, change_board)
-    print(cnt)
+    solve(init, 0, 0)
+    print(ans)
