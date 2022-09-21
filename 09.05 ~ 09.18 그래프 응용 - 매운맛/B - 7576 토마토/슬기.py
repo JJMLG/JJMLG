f6e2c415bca576293f48908907fb89900c596504
문슬기 <tmfrlghtjr@gmail.com>
2022-09-21 23:30:44 +0900

    upload

--- 09.05 ~ 09.18 그래프 응용 - 매운맛/B - 7576 토마토/슬기.py
+++ 09.05 ~ 09.18 그래프 응용 - 매운맛/B - 7576 토마토/슬기.py
@@ -1,2 +1,21 @@
 import sys
 sys.stdin = open('input.txt')
+
+m, n = map(int, input().split())
+ground = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [[0] * m for _ in range(n)]
+# print(visited)
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+# chain = [[0, 0], [0, 1], []]
+
+def bfs(x, y):
+    pass
+
+
+for i in range(n):
+    for j in range(m):
+        if ground[i][j] == 1:
+            bfs(i, j)
