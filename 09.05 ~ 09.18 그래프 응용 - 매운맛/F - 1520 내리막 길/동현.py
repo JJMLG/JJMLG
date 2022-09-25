f7f42ca7481ce2d55299537b4e02ad794e955687
ehdgus <rhkrehd619@naver.com>
2022-09-25 19:22:37 +0900

    sol

--- 09.05 ~ 09.18 그래프 응용 - 매운맛/F - 1520 내리막 길/동현.py
+++ 09.05 ~ 09.18 그래프 응용 - 매운맛/F - 1520 내리막 길/동현.py
@@ -1,29 +1,32 @@
+
 import sys
-from collections import deque
-import copy 
-from pprint import pprint
+from collections import deque   
 sys.stdin = open('input.txt')
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
+def dfs(x,y):
+ 
+    if x == n-1 and y == m-1 :
+        return 1
+    
+    if visited[x][y] != -1:
+        return visited[x][y]
+    
+    ways = 0
+    for k in range(4):
+        nx = x +dx[k]
+        ny = y +dy[k]
+        if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] < arr[x][y]:
+            ways += dfs(nx,ny)
+    
+    visited[x][y] = ways
+    return visited[x][y]
+
 n,m = map(int,input().split())
 arr = [list(map(int,input().split())) for _ in range(n)]
 visited = [[-1]*m for _ in range(n)]
-def dfs(x,y):
-    global ans
-    if x == n-1 and y == m-1 :
-        return 1
-    if visited[x][y] == -1:
-        visited[x][y] = 0
 
-        for k in range(4):
-            nx = x +dx[k]
-            ny = y +dy[k]
-            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] < arr[x][y]:
-                visited[x][y] += dfs(nx,ny)
-        
-    return visited[x][y]
+    
 
-ans = 0 
-print(dfs(0,0))
