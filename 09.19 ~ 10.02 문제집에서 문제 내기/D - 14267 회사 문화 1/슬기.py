ea5b9523e11faea03086928b06a28ac16e6a7612
문슬기 <tmfrlghtjr@gmail.com>
2022-10-01 16:55:06 +0900

    seulgi upload

--- 09.19 ~ 10.02 문제집에서 문제 내기/D - 14267 회사 문화 1/슬기.py
+++ 09.19 ~ 10.02 문제집에서 문제 내기/D - 14267 회사 문화 1/슬기.py
@@ -1,2 +1,9 @@
 import sys
 sys.stdin = open('input.txt')
+
+n, m = map(int, input().split())
+num = list(map(int, input().split()))
+
+for _ in range(m):
+    i, w = list(map(int, input().split()))
+    # print(i, w)
