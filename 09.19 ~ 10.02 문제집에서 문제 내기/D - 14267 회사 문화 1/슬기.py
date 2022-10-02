c69c54e252b911975c6b1b2140782a28b1dec320
문슬기 <tmfrlghtjr@gmail.com>
2022-10-02 23:52:52 +0900

    upload

--- 09.19 ~ 10.02 문제집에서 문제 내기/D - 14267 회사 문화 1/슬기.py
+++ 09.19 ~ 10.02 문제집에서 문제 내기/D - 14267 회사 문화 1/슬기.py
@@ -4,6 +4,23 @@ sys.stdin = open('input.txt')
 n, m = map(int, input().split())
 num = list(map(int, input().split()))
 
+# num_index = list(range(1, len(num)+1))
+# print(num_index)
+
+dp = [0] * (n+10)
+
+
+graph = [[] for _ in range(n+1)]
+# print(graph)
+
 for _ in range(m):
     i, w = list(map(int, input().split()))
     # print(i, w)
+    graph[i].append(w)
+print(graph[3][0])
+
+dp[0] = 0
+dp[1] = dp[1] + graph[1][0]
+dp[2] = dp[1] + graph[2][0]
+dp[3] = dp[2] + graph[3][0]
+print(dp[3])
