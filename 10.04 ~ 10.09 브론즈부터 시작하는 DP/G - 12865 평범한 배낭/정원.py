242f0cd2792dec07199d5dee1b1ea73afd5172b4
정원 <sorrow4468@gmail.com>
2022-10-05 05:11:29 +0900

    정원 10.04 ~ 10.09 브론즈부터 시작하는 DP G - 12865 평범한 배낭

---
+++ 10.04 ~ 10.09 브론즈부터 시작하는 DP/G - 12865 평범한 배낭/정원.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().rstrip().split())
+items = [tuple(map(int, input().rstrip().split())) for _ in range(N)]
+dp = [0]*(K+1)
+for item in items:
+    weight, value = item
+    for i in range(K, weight-1, -1): # top-down
+        dp[i] = max(dp[i], dp[i-weight]+value)
+print(dp[-1])
+
+"""
+top-down 방식의 문제이다
+현재 무게(dp[i])에서 얻을 수 있는 가장 큰 가치를
+아래 두 값 중 최대값으로 갱신한다
+1. 이미 구한 현재 무게의 가치 최대치(dp[i])
+2. 현재 물건의 무게만큼 더 담을 수 있던 시점의 가치 최대치에 현재 물건의 가치를 더한 값
+ex) dp[7] = max(dp[7], dp[3]+items[4][value])
+ex2) dp[6] = max(dp[6], dp[0]+items[6][value])
+"""
+
+# https://www.acmicpc.net/problem/12865
