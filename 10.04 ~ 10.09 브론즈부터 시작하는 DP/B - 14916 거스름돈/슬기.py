c3ac50138b3aa951c2eebced9459685fb2149c55
문슬기 <tmfrlghtjr@gmail.com>
2022-10-07 00:15:32 +0900

    upload

--- 10.04 ~ 10.09 브론즈부터 시작하는 DP/B - 14916 거스름돈/슬기.py
+++ 10.04 ~ 10.09 브론즈부터 시작하는 DP/B - 14916 거스름돈/슬기.py
@@ -1,37 +1,19 @@
 import sys
 sys.stdin = open('input.txt')
+input = sys.stdin.readline
 
 n = int(input())
 
-change = {2: 0, 5: 0}
-# print(change)
+dp = [-1] * (n+5)
 
-dp = [2, 5]
+dp[2] = 1
+dp[4] = 2
+dp[5] = 1
+dp[6] = 3
+dp[7] = 2
+dp[8] = 4
 
-
-while True:
-    i = n
-    print(i)
-    if i == 1:
-        print(-1)
-        break
-
-    elif i % 5 == 0:
-        change[5] += 1
-        i = i - 5
-
-    elif i % 2 == 0:
-        change[2] += 1
-        i = i - 2
-
-    elif i % 7 == 0:
-        change[2] += 1
-        change[5] += 1
-        i = i - 7
-
-    else:
-        print(-1)
-        # break
-
-print(change)
+for i in range(9, n+1):
+    dp[i] = min(dp[i-2], dp[i-5]) + 1
+print(dp[n])
 
