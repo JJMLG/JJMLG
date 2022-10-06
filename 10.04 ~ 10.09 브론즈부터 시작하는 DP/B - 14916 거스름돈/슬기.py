c6114b80c7cd5335467c5e4f8639617a006b53dd
문슬기 <tmfrlghtjr@gmail.com>
2022-10-06 23:32:46 +0900

    upload

--- 10.04 ~ 10.09 브론즈부터 시작하는 DP/B - 14916 거스름돈/슬기.py
+++ 10.04 ~ 10.09 브론즈부터 시작하는 DP/B - 14916 거스름돈/슬기.py
@@ -9,3 +9,29 @@ change = {2: 0, 5: 0}
 dp = [2, 5]
 
 
+while True:
+    i = n
+    print(i)
+    if i == 1:
+        print(-1)
+        break
+
+    elif i % 5 == 0:
+        change[5] += 1
+        i = i - 5
+
+    elif i % 2 == 0:
+        change[2] += 1
+        i = i - 2
+
+    elif i % 7 == 0:
+        change[2] += 1
+        change[5] += 1
+        i = i - 7
+
+    else:
+        print(-1)
+        # break
+
+print(change)
+
