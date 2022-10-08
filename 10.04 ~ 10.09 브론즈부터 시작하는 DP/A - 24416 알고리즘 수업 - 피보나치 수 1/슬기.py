cfaf3d7c933bbb2269871fd242fae78ca3a90e52
문슬기 <tmfrlghtjr@gmail.com>
2022-10-08 23:28:36 +0900

    seulgi upload

--- 10.04 ~ 10.09 브론즈부터 시작하는 DP/A - 24416 알고리즘 수업 - 피보나치 수 1/슬기.py
+++ 10.04 ~ 10.09 브론즈부터 시작하는 DP/A - 24416 알고리즘 수업 - 피보나치 수 1/슬기.py
@@ -1,2 +1,14 @@
 import sys
 sys.stdin = open('input.txt')
+
+n = int(input())
+
+
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    elif n > 2:
+        return fib(n - 1) + fib(n - 2)
+    return 0
+
+print(fib(n), n-2)
--- 10.04 ~ 10.09 브론즈부터 시작하는 DP/B - 14916 거스름돈/슬기.py
+++ 10.04 ~ 10.09 브론즈부터 시작하는 DP/B - 14916 거스름돈/슬기.py
@@ -1,19 +1,38 @@
 import sys
 sys.stdin = open('input.txt')
-input = sys.stdin.readline
+# input = sys.stdin.readline
 
 n = int(input())
 
-dp = [-1] * (n+5)
+# dp = [-1] * (n+5)
+#
+# dp[2] = 1
+# dp[4] = 2
+# dp[5] = 1
+# dp[6] = 3
+# dp[7] = 2
+# dp[8] = 4
+#
+# for i in range(9, n+1):
+#     dp[i] = min(dp[i-2], dp[i-5]) + 1
+# print(dp[n])
 
-dp[2] = 1
-dp[4] = 2
-dp[5] = 1
-dp[6] = 3
-dp[7] = 2
-dp[8] = 4
+cnt = 0
+i = 0
 
-for i in range(9, n+1):
-    dp[i] = min(dp[i-2], dp[i-5]) + 1
-print(dp[n])
+while True:
+    if n % 5 == 0:
+        cnt += n // 5
+        break
+    else:
+        n -= 2
+        cnt += 1
+
+    if n < 0:
+        break
+
+if n < 0:
+    print(-1)
+else:
+    print(cnt)
 
