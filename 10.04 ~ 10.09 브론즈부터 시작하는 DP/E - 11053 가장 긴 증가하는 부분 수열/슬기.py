9e022222723ec4f2b6368ef7bb1ffbb3a499fecc
문슬기 <tmfrlghtjr@gmail.com>
2022-10-10 00:16:52 +0900

    upload seulgi

--- 10.04 ~ 10.09 브론즈부터 시작하는 DP/E - 11053 가장 긴 증가하는 부분 수열/슬기.py
+++ 10.04 ~ 10.09 브론즈부터 시작하는 DP/E - 11053 가장 긴 증가하는 부분 수열/슬기.py
@@ -1,2 +1,15 @@
 import sys
 sys.stdin = open('input.txt')
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+dp = [0] * (n+5)
+
+for i in range(n):
+    for j in range(i):
+        if arr[j] < arr[i] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+
+print(max(dp))
