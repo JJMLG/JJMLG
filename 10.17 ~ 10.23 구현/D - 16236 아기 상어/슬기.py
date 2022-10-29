ba693723811a549b31ca2c87451276d6dc52f02f
문슬기 <tmfrlghtjr@gmail.com>
2022-10-29 12:08:18 +0900

    upload seulgi

--- 10.17 ~ 10.23 구현/D - 16236 아기 상어/슬기.py
+++ 10.17 ~ 10.23 구현/D - 16236 아기 상어/슬기.py
@@ -2,72 +2,70 @@ import sys
 sys.stdin = open('input.txt')
 from collections import deque
 
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-n = int(input())
-
-time = 0
-baby_shark = 2
+cnt = 0
+x, y, baby_shark = 0, 0, 2
 
 
-def bfs(x, y):
-    visited = [[0] * n for _ in range(n)]
-    visited[x][y] = 1
-    eat = []
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 9:
+            x = i
+            y = j
+
+
+def bfs(x, y, shark_size):
     dist = [[0] * n for _ in range(n)]
+    visited = [[0] * n for _ in range(n)]
+
     q = deque()
-    q.append([x, y])
+    q.append((x, y))
+    visited[x][y] = 1
+    tmp = []
 
     while q:
-        t, u = q.popleft()
+        a, b = q.popleft()
 
         for k in range(4):
-            nx = t + dx[k]
-            ny = u + dy[k]
+            nx = a + dx[k]
+            ny = b + dy[k]
 
             if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == 0:
-                if sea[nx][ny] <= sea[x][y] or sea[nx][ny] == 0:
-                    q.append([nx, ny])
+                if arr[nx][ny] <= shark_size:
+                    q.append((nx, ny))
                     visited[nx][ny] = 1
-                    dist[nx][ny] = dist[t][u] + 1
+                    dist[nx][ny] = dist[a][b] + 1
+                    if arr[nx][ny] < shark_size and arr[nx][ny] != 0:
+                        tmp.append((nx, ny, dist[nx][ny]))
 
-                if sea[nx][ny] < sea[x][y] and sea[nx][ny] != 0:
-                    eat.append([nx, ny, dist[nx][ny]])
-
-    if not eat:
-        return -1, -1, -1
-    eat.sort(key = lambda x : (x[2], x[0], x[1]))
-    return eat[0][0], eat[0][1], eat[0][2]
+    return sorted(tmp, key=lambda x: (-x[2], -x[0], -x[1]))
 
 
-sea = []
-for i in range(n):
-    arr = list(map(int, input().split()))
-    sea.append(arr)
-    for j in range(n):
-        if arr[j] == 9:
-            sea[i][j] = 2
-            start = [i, j]
-
-exp = 0
 cnt = 0
+result = 0
 
 while True:
-    i, j = start[0], start[1]
-    nx, ny, dist = bfs(i, j)
+    shark = bfs(x, y, baby_shark)
 
-    if nx == -1:
+    if len(shark) == 0:
         break
 
-    sea[nx][ny] = sea[i][j]
-    sea[i][j] = 0
-    start = [nx, ny]
-    exp += 1
+    nx, ny, dist = shark.pop()
 
-    if exp == sea[nx][ny]:
-        exp = 0
-        sea[nx][ny] += 1
-    cnt += dist
+    result += dist
+    arr[x][y], arr[nx][ny] = 0, 0
 
-print(cnt)
+    x, y = nx, ny
+    cnt += 1
+    if cnt == baby_shark:
+        baby_shark += 1
+        cnt = 0
+
+print(result)
