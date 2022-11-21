f3557c2523a74042496314c71027b36facf090ad
문슬기 <tmfrlghtjr@gmail.com>
2022-11-21 22:55:15 +0900

    upload seulgi

--- 11.21 ~ 11.27 11월 마지막/C - 21921 블로그/슬기.py
+++ 11.21 ~ 11.27 11월 마지막/C - 21921 블로그/슬기.py
@@ -1,4 +1,19 @@
 import sys
 sys.stdin = open('input.txt')
 
+n, m = map(int, input().split())
+day = list(map(int, input().split()))
 
+tmp = sum(day[:m])
+ans = [tmp]
+
+for i in range(m, n):
+    tmp -= day[i-m]
+    tmp += day[i]
+    ans.append(tmp)
+
+if sum(ans) == 0:
+    print('SAD')
+else:
+    print(max(ans))
+    print(ans.count(max(ans)))
