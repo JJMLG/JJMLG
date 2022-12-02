4059fca264c481e2d439e9b55687eee5d7d39def
hyejin <87624430+hy2jin@users.noreply.github.com>
2022-12-03 02:32:16 +0900

    미로

--- 11.27 ~ 12.04 12월 1주차/C - 16954 움직이는 미로 탈출/혜진.py
+++ 11.27 ~ 12.04 12월 1주차/C - 16954 움직이는 미로 탈출/혜진.py
@@ -4,10 +4,10 @@ d = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (0, 0
 def bfs():
     new = []
     for r, c in Q:
-        if r == 0:
+        if r == 0:              # 0행이면 제일 오른쪽에 갈 수 있다
             return 1
 
-        if arr[r][c] == ".":
+        if arr[r][c] == ".":    # 움직인 뒤 벽이 내려왔으면 pass 아니면 ㄱㄱ
             for dr, dc in d:
                 nr, nc = r + dr, c + dc
                 if 0 <= nr < 8 and 0 <= nc < 8 and arr[nr][nc] == ".":
@@ -24,4 +24,4 @@ while Q and not ans:
 
     arr = ["........"] + arr[:7]
 
-print(ans)
+print(ans)                      # 모든 경우에 벽이라서(10번줄) Q가 끝나버리면 0이 출력
