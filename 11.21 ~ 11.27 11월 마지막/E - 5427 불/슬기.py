322525287b241bc75d25df38351e3eaff3a42249
문슬기 <tmfrlghtjr@gmail.com>
2022-12-05 22:09:40 +0900

    upload seulgi

--- 11.21 ~ 11.27 11월 마지막/E - 5427 불/슬기.py
+++ 11.21 ~ 11.27 11월 마지막/E - 5427 불/슬기.py
@@ -3,67 +3,69 @@ sys.stdin = open('input.txt')
 from pprint import pprint
 from collections import deque
 
-dx = [-1, -1, 0, 0]
+dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
-def fire_bfs(x, y):
-    fq = deque()
-    fq.append([x, y])
-    visited[x][y] = 2
-
-    while fq:
+def fire_bfs():
+    # while fq:
+    for _ in range(len(fq)):
         a, b = fq.popleft()
 
         for k in range(4):
             nx = a + dx[k]
             ny = b + dy[k]
 
-            if 0 <= nx < w and 0 <= ny < h and visited[nx][ny] == 0:
-                # time += 1
-                visited[nx][ny] = 2
-                fq.append([nx, ny])
-                # print(time)
-    # pprint(visited)
+            if 0 <= nx < h and 0 <= ny < w:
+                if building[nx][ny] != '#' and building[nx][ny] != '*':
+                    building[nx][ny] = '*'
+                    fq.append([nx, ny])
 
-def bfs(x, y):
-    global time
-    q = deque()
-    q.append([x, y])
-    visited[x][y] = 1
+def bfs():
+    go = 0
 
-    while q:
+    # while q:
+    for _ in range(len(q)):
         a, b = q.popleft()
 
         for k in range(4):
             nx = a + dx[k]
             ny = b + dy[k]
 
-            if 0 <= nx < h and 0 <= ny < w and visited[nx][ny] == 0:
-                time += 1
-                visited[nx][ny] = 1
-                q.append([nx, ny])
-                # print(time)
+            if 0 <= nx < h and 0 <= ny < w:
+                if visited[nx][ny] == 0 and building[nx][ny] != '#' and building[nx][ny] != '*':
+                    go = 1
+                    visited[nx][ny] = visited[a][b] + 1
+                    q.append([nx, ny])
+            else:
+                return visited[a][b]
+
+    if not go:
+        return 'IMPOSSIBLE'
 
 
 t = int(input())
 for _ in range(t):
     w, h = map(int, input().split())
-    building = [input() for _ in range(h)]
-    # pprint(building)
-    visited = [[0] * w for _ in range(h)]
-    # print(visited)
-    time = 1
+    building = []
+    fq = deque()
+    q = deque()
     for i in range(h):
+        building.append(list(input().strip()))
+        # print(building)
         for j in range(w):
-            # print(i, j)
-            if building[i][j] == '#':
-                visited[i][j] = 1
-            if building[i][j] == '@':
-                bfs(i, j)
             if building[i][j] == '*':
-                visited[i][j] = 2
-                # fire_bfs(i, j)
+                fq.append([i, j])
+            if building[i][j] == '@':
+                q.append([i, j])
+    visited = [[0] * w for _ in range(h)]
+    visited[q[0][0]][q[0][1]] = 1
 
-    print(visited)
-    print(time)
+    # print(visited)
+    res = 0
+    while True:
+        fire_bfs()
+        res = bfs()
+        if res:
+            break
+    print(res)
