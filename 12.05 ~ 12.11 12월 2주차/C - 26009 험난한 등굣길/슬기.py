3008f2724efa5de9887da8263a97dde45f53383d
문슬기 <tmfrlghtjr@gmail.com>
2022-12-18 23:05:45 +0900

    upload

--- 12.05 ~ 12.11 12월 2주차/C - 26009 험난한 등굣길/슬기.py
+++ 12.05 ~ 12.11 12월 2주차/C - 26009 험난한 등굣길/슬기.py
@@ -6,34 +6,36 @@ sys.stdin = open('input.txt')
 dx = [1, -1, 0, 0]
 dy = [0, 0, -1, 1]
 
+dx2 = [1, -1, 0, 0, 1, 1, -1, -1]
+dy2 = [0, 0, -1, 1, -1, 1, -1, 1]
 
 def bfs(x, y):
-    global visited, cnt
+    global visited, flag, cnt
     q = deque()
     q.append([x, y])
-    # local[x][y] += 1
-    # print(x,y)
-    # visited[x][y] += 1
+    # visited[x][y] = 1
+
     while q:
-        # print(q)
-        f, s = q.popleft()
+        # print(t)
+        for _ in range(len(q)):
+            t = q.popleft()
+            f = t[0]
+            s = t[1]
 
-        # print(f, s, '?')
-        for l in range(4):
-            nnx = f + dx[l]
-            nny = s + dy[l]
+            for l in range(4):
+                nnx = f + dx[l]
+                nny = s + dy[l]
 
-            if nnx == n and nny == m:
-                # cnt += 1
-                return cnt
+                if nnx == n and nny == m:
+                    # cnt += 1
+                    flag = 1
+                    return cnt + 1
 
-            if 0 <= nnx <= n and 0 <= nny <= m and local[nnx][nny] == 0:
-                visited[nnx][nny] = visited[f][s] + 1
-                local[nnx][nny] = 1
-                cnt += 1
-                bfs(nnx, nny)
-                pprint(local)
-                pprint(visited)
+                if 0 < nnx <= n and 0 < nny <= m and local[nnx][nny] == 0:
+                    local[nnx][nny] = 1
+                    q.append([nnx, nny])
+
+        cnt += 1
 
     return cnt
 
@@ -46,23 +48,31 @@ local[1][1] = 3 # 내위치
 local[n][m] = 2 # 학교
 visited = [[0] * (m+1) for _ in range(n+1)]
 # print(visited)
+flag = 0
 for _ in range(k):
     r, c, d = map(int, input().split())
     local[r][c] = 1
     # print(local)
-    for _ in range(d):
-        for k in range(4):
-            nx = r + dx[k]
-            ny = c + dy[k]
+    for i in range(1, d+1):
+        for k in range(8):
+            nx = r + dx2[k] * i
+            ny = c + dy2[k] * i
             # print(nx, ny)
-            if 0 <= nx <= n and 0 <= ny <= m and local[nx][ny] == 0:
+            if 0 <= nx <= n and 0 <= ny <= m \
+                    and local[nx][ny] == 0 \
+                    and abs(r-nx) + abs(c-ny) <= d:
                 local[nx][ny] = 1
                 # print(nx, ny)
-pprint(local)
+# pprint(local)
 res = 0
 cnt = 0
 for i in range(n):
     for j in range(m):
         if local[i][j] == 3:
             res = bfs(i, j)
-print(res)
+
+if flag == 0:
+    print('NO')
+else:
+    print('YES')
+    print(res)
