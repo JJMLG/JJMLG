25e3bd13ae8990c9423624fa84961989c0631af5
daedaem <tg8685@gmail.com>
2023-01-15 20:09:12 +0900

    docs: add 백준 26152_플래피 버드 스코어링-해성

--- 01.09 ~ 01.15 2023년 계묘년 첫빠따쓰 문제/A - 26152 플래피 버드 스코어링/해성.cpp
+++ 01.09 ~ 01.15 2023년 계묘년 첫빠따쓰 문제/A - 26152 플래피 버드 스코어링/해성.cpp
@@ -2,44 +2,38 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-struct INFO{
-	int idx, h;
-	
-};
-vector<INFO> arr;
+vector<int> arr;
 vector<int>birds;
 int N, Q;
 
-bool COMP(INFO b, INFO a) {
-	return a.h > b.h;
-}
 void input() {
 	cin >> N;
-	for (int i = 1; i <= N; i++) {
+	for (int i = 0; i < N; i++) {
 		int h; cin >> h;
-		arr.push_back({ i, h });
+		arr.push_back(h);
 	}
-	for (int i = 1; i <= N; i++) {
+	for (int i = 0; i < N; i++) {
 		int h; cin >> h;
-		arr[i - 1].h -= h;
+		arr[i] -= h;
+		if (i > 0) {
+			if (arr[i - 1] < arr[i]) arr[i] = arr[i - 1];
+		}
 	}
-
-	sort(arr.begin(), arr.end(), COMP);
 
 	cin >> Q;
-	birds.resize(Q);
+	birds.resize(Q+1);
 	for (int i = 0; i < Q; i++) {
 		cin >> birds[i];
 	}
 }
-int bs(int bidx) {
+int bs(int birdH) {
 	int start = 0;
-	int end = N;
-	int rangeidx = 2000000001;
+	int end = N-1;
+	int rangeidx = -1;
 	while (start <= end) {
 		int mid = (start + end) / 2;
-		if (arr[mid].h < bidx) {
-			if (rangeidx > mid) rangeidx = mid;
+		if (arr[mid] >= birdH) {
+			if (rangeidx < mid) rangeidx = mid;
 			start = mid + 1;
 		}
 		else {
@@ -48,18 +42,12 @@ int bs(int bidx) {
 	}
 	return rangeidx;
 }
-bool comp(INFO a, INFO b) {
-	return a.idx < b.idx;
-}
 void solve() {
 	for (int i = 0; i < Q; i++) {
 		int ret = bs(birds[i]);
-		cout << ret << '\n';
-		/*if (ret == -1) cout<< N << '\n';
-		else {
-			auto mins = min_element(arr.begin(), arr.begin() + ret+1, comp);
-		//	cout << mins->idx-1 << "\n";*/
-		//}
+		//cout << ret << "\n";
+		if (ret == -1) cout << 0 << '\n';
+		else cout << ret+1 << '\n';
 	}
 }
 int main(void) {
