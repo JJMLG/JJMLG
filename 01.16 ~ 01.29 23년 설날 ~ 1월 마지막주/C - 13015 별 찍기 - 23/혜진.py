f3a2fae15a20973f9917ed9d0fe5a0f487c0afe7
hyejin <87624430+hy2jin@users.noreply.github.com>
2023-01-25 23:48:20 +0900

    별찍기23

---
+++ 01.16 ~ 01.29 23년 설날 ~ 1월 마지막주/C - 13015 별 찍기 - 23/혜진.py
@@ -0,0 +1,21 @@
+N = int(input())
+
+# 첫줄
+print('*' * N + ' ' * (2 * (N - 1) - 1) + '*' * N)
+
+arr = []
+tmp = '*' + ' ' * (N - 2)  + '*'
+
+for i in range(1, N - 1):
+    arr.append(' ' * i + tmp + ' ' * (2 * (N - 1 - i) - 1) + tmp)
+    print(arr[i - 1])
+
+# 한가운데
+print(' ' * (N - 1) + tmp + ' ' * (N - 2) + '*')
+
+L = len(arr)
+for i in range(L):
+    print(arr[L - i - 1])
+
+# 막줄
+print('*' * N + ' ' * (2 * (N - 1) - 1) + '*' * N)
