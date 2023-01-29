af0bcd0be71b6d260fe363e04e5ae63aaa7c4748
daedaem <tg8685@gmail.com>
2023-01-29 19:18:46 +0900

    docs: add 01.16 ~ 01.29 23년 설날 ~ 1월 마지막주(해성)

--- 01.16 ~ 01.29 23년 설날 ~ 1월 마지막주/E - 5547 일루미네이션/해성.cpp
+++ 01.16 ~ 01.29 23년 설날 ~ 1월 마지막주/E - 5547 일루미네이션/해성.cpp
@@ -16,6 +16,7 @@ POS DIR[2][6] = {
 	{ {-1,0}, {-1,1},{0,1},{1,1},{1,0},{0,-1} } 
 };
 queue<POS> q;
+
 void input() {
 	cin >> W >> H;
 	for (int r = 1; r <= H; r++) {
@@ -40,39 +41,16 @@ void makeSide() {
 				newdata.c += DIR[0][idx].c;
 			}
 			if (newdata.r < 0 || newdata.c < 0 || newdata.r > H + 1 || newdata.c > W + 1) continue;
-			//if (visit[newdata.r][newdata.c]) continue;
-			//visit[newdata.r][newdata.c] = 1;
 			if (maps[newdata.r][newdata.c]) continue;
 			maps[newdata.r][newdata.c] = 2;
 			q.push(newdata);
 		}
 	}
 }
-//int isInRound(int r, int c) {
-//	for (int idx = 0; idx < 6; idx++) {
-//		int dy, dx;
-//		if (r % 2) {
-//			dy = r + DIR[1][idx].r;
-//			dx = c + DIR[1][idx].c;
-//		}
-//		else {
-//			dy = r + DIR[0][idx].r;
-//			dx = c + DIR[0][idx].c;
-//		}
-//
-//		if (dy < 1 || dx < 1 || dy > H || dx > W) return 0;
-//		if (!maps[dy][dx]) return 0;
-//	}
-//	return 1;
-//}
 void makeMap() {
 	for (int r = 0; r <= H+1; r++) {
 		for (int c = 0; c <= W+1; c++) {
 			if (maps[r][c] == 0) maps[r][c] = 1;
-			//int ret = isInRound(r, c);
-			/*if (ret) {
-				maps[r][c] = 1;
-			}*/
 		}
 	}
 }
@@ -93,15 +71,6 @@ int checkNear(int r, int c) {
 	}
 	return cnt;
 }
-void print() {
-	cout << '\n';
-	for (int r = 1; r <= H; r++) {
-		for (int c = 1; c <= W; c++) {
-			cout << maps[r][c] << " ";
-		}
-		cout << '\n';
-	}
-}
 int calcCirm() {
 	int total = 0;
 	for (int r = 1; r <= H; r++) {
@@ -113,15 +82,6 @@ int calcCirm() {
 		}
 	}
 	return total;
-}
-
-void printvisit() {
-	for (int r = 0; r <= H + 1; r++) {
-		for (int c = 0; c <= W + 1; c++) {
-			cout << visit[r][c] << " ";
-		}
-		cout << '\n';
-	}
 }
 void solve() {
 	makeSide();
