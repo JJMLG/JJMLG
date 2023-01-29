7e9c5712c1cac00cb66fbc5621b5b4a08ccb6891
문슬기 <tmfrlghtjr@gmail.com>
2023-01-29 20:26:10 +0900

    upload seulgi

--- 01.16 ~ 01.29 23년 설날 ~ 1월 마지막주/D - 1092 배/슬기.py
+++ 01.16 ~ 01.29 23년 설날 ~ 1월 마지막주/D - 1092 배/슬기.py
@@ -3,5 +3,10 @@ sys.stdin = open('input.txt')
 
 n = int(input())
 crane = list(map(int, input().split()))
+crane.sort(reverse=True)
 m = int(input())
 box = list(map(int, input().split()))
+box.sort(reverse=True)
+print(crane, box)
+
+
