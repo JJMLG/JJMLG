47f6c61df34d512d72aca9769e9aa41184a77b9b
문슬기 <tmfrlghtjr@gmail.com>
2023-02-01 23:53:24 +0900

    upload seulgi

--- 01.29 ~ 02.05 23년 1월 끝 & 2월 시작/A - 4195 친구 네트워크/슬기.py
+++ 01.29 ~ 02.05 23년 1월 끝 & 2월 시작/A - 4195 친구 네트워크/슬기.py
@@ -1,2 +1,25 @@
 import sys
 sys.stdin = open('input.txt')
+
+t = int(input())
+
+for _ in range(t):
+    f = int(input())
+    network = {}
+    cnt = 2
+    for _ in range(f):
+        fr1, fr2 = input().split()
+        if fr1 not in network:
+            network[fr1] = 1
+        else:
+            network[fr1] += 1
+            cnt += 1
+
+        if fr2 not in network:
+            network[fr2] = 1
+        else:
+            network[fr2] += 1
+            cnt += 1
+
+        print(network)
+        # print(cnt)
