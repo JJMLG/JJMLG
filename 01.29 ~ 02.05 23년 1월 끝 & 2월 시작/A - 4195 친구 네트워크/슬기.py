d63c7df10bf735b54bd74f42174a6d22304de463
문슬기 <tmfrlghtjr@gmail.com>
2023-02-02 22:22:38 +0900

    upload seulgi

--- 01.29 ~ 02.05 23년 1월 끝 & 2월 시작/A - 4195 친구 네트워크/슬기.py
+++ 01.29 ~ 02.05 23년 1월 끝 & 2월 시작/A - 4195 친구 네트워크/슬기.py
@@ -6,20 +6,46 @@ t = int(input())
 for _ in range(t):
     f = int(input())
     network = {}
-    cnt = 2
+    cnt = 0
     for _ in range(f):
         fr1, fr2 = input().split()
         if fr1 not in network:
             network[fr1] = 1
         else:
             network[fr1] += 1
-            cnt += 1
+            # cnt += 1
 
         if fr2 not in network:
             network[fr2] = 1
         else:
             network[fr2] += 1
-            cnt += 1
+            # cnt += 1
 
         print(network)
-        # print(cnt)
+        print(cnt)
+
+
+"""
+2
+3
+3
+3
+
+1
+7
+a b
+b c
+c a
+d e
+e d
+d e
+a b
+
+2
+3
+3
+2
+2
+2
+3
+"""
