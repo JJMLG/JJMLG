f3785e0b7c2b1452f3f69087000c6ab6e83fc921
문슬기 <tmfrlghtjr@gmail.com>
2023-02-05 21:39:04 +0900

    upload seulgi

--- 01.29 ~ 02.05 23년 1월 끝 & 2월 시작/B - 1956 운동/슬기.py
+++ 01.29 ~ 02.05 23년 1월 끝 & 2월 시작/B - 1956 운동/슬기.py
@@ -1,33 +1,15 @@
 import sys
 sys.stdin = open('input.txt')
-from collections import deque
 
-def bfs(x):
-    q = deque()
-    q.append(x)
-    # print(q)
-    while q:
-        t = q.popleft()
-        # print(t)
 
-        for i in t:
-            # print(i)
-            if visited[i[0]-1] == -1:
-                visited[i[0]-1] = visited[i[1]]
-                print(visited)
-
+INF = int(1e9)
 
 v, e = map(int, input().split())
 
 graph = [[] for _ in range(v+1)]
 cnt = 0
-visited = [-1] * (v+1)
-
+visited = [INF] * (v+1)
 # print(visited)
 for _ in range(e):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
-
-for i in range(1, len(graph)):
-    bfs(graph[i])
-# print(graph)
