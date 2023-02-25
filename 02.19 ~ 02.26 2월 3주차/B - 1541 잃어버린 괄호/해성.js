51c5bef6f8c5199ea8bc6c816c7fe4cf568967e1
daedaem <tg8685@gmail.com>
2023-02-25 23:38:17 +0900

    docs: add 백준 B - 1541 잃어버린 괄호(해성)

--- 02.19 ~ 02.26 2월 3주차/B - 1541 잃어버린 괄호/해성.js
+++ 02.19 ~ 02.26 2월 3주차/B - 1541 잃어버린 괄호/해성.js
@@ -1,55 +1,63 @@
-const INPUT = require("fs").readFileSync("./input.txt").toString().trim();
-let answer;
-let word = "";
-const NUM = [];
-const OPE = INPUT.split("").filter((val, idx) => {
-  if (val == "+" || val == "-") {
-    if (word != "") {
-      const newel = Number(word);
-      word = "";
-      NUM.push(newel);
-      //   return newel;
-      return val;
-    }
-  } else {
-    word += val;
-  }
-});
+const solve1 = () => {
+  const INPUT = require("fs")
+    .readFileSync("./input.txt")
+    .toString()
+    .trim()
+    .split("-")
+    .map((el) =>
+      el
+        .split("+")
+        .map(Number)
+        .reduce((sums, val) => sums + val)
+    );
+  const result = INPUT[0] * 2 + INPUT.reduce((sums, val) => sums - val, 0);
+  console.log(result);
+};
+solve1();
 
-if (word != "") NUM.push(Number(word));
+// solve2 = () => {
+//   const INPUT = require("fs").readFileSync("/dev/stdin").toString().trim();
+//   let answer;
+//   let word = "";
+//   const NUM = [];
+//   const OPE = INPUT.split("").filter((val, idx) => {
+//     if (val == "+" || val == "-") {
+//       if (word != "") {
+//         const newel = Number(word);
+//         word = "";
+//         NUM.push(newel);
+//         return val;
+//       }
+//     } else {
+//       word += val;
+//     }
+//   });
 
-let sum = NUM.shift();
-let startIdx = 0;
-let tempSum = 0;
-while (startIdx < NUM.length) {
-  if (OPE[startIdx] == "+") {
-    tempSum += NUM[startIdx++];
-  } else {
-    sum += tempSum;
-    tempSum = NUM[startIdx++];
-    while (OPE[startIdx] != "-" && startIdx < OPE.length) {
-      tempSum += NUM[startIdx++];
-    }
-    sum -= tempSum;
-    tempSum = 0;
-  }
-}
-if (tempSum) {
-  if (OPE.pop() == "-") {
-    sum -= tempSum;
-  } else {
-    sum += tempSum;
-  }
-}
-console.log(sum);
-// ++ // 마지막 + 이면 +temppsum
-// + - //마지막 -이면 -tempsum
-//   --
-// -+
-// if (tempSum) {
+//   if (word != "") NUM.push(Number(word));
 
-// }
-
-// for (let i = 0; i < NUM.length; i++) {}
-
-// console.log(OPE, NUM);
+//   let sum = NUM.shift();
+//   let startIdx = 0;
+//   let tempSum = 0;
+//   while (startIdx < NUM.length) {
+//     if (OPE[startIdx] == "+") {
+//       tempSum += NUM[startIdx++];
+//     } else {
+//       sum += tempSum;
+//       tempSum = NUM[startIdx++];
+//       while (OPE[startIdx] != "-" && startIdx < OPE.length) {
+//         tempSum += NUM[startIdx++];
+//       }
+//       sum -= tempSum;
+//       tempSum = 0;
+//     }
+//   }
+//   if (tempSum) {
+//     if (OPE.pop() == "-") {
+//       sum -= tempSum;
+//     } else {
+//       sum += tempSum;
+//     }
+//   }
+//   console.log(sum);
+// };
+// solve2();
