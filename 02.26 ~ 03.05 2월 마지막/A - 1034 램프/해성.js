09bbaf2859fa1b99e08cc92322354d587d21d789
daedaem <tg8685@gmail.com>
2023-03-03 12:46:13 +0900

    docs: add 02.26 ~ 03.05 2월 마지막 - A 1034 램프(해성)

--- 02.26 ~ 03.05 2월 마지막/A - 1034 램프/해성.js
+++ 02.26 ~ 03.05 2월 마지막/A - 1034 램프/해성.js
@@ -6,40 +6,53 @@ const IN = require("fs")
 
 const [N, M] = IN.shift().split(" ").map(Number);
 const K = Number(IN.pop());
-const pattern = IN.map((el) => el.trim());
-const patternMap = {};
-
-let answer = 0;
-
-const find = (el) => {
-  const count = el.split("").reduce((init, val) => {
-    return init + !Number(val);
-  }, 0);
-  return count;
-};
-const solve = () => {
-  pattern.map((el) => {
-    if (patternMap[el] && patternMap[el].count != -1) {
-      patternMap[el].count++;
-      if (answer < patternMap[el].count) answer = patternMap[el].count;
-    } else {
-      const count = find(el);
-      if (count % 2 == K % 2 && count <= K) {
-        patternMap[el] = {
-          zeroCount: count,
-          count: 1,
-        };
-        if (answer < patternMap[el].count) {
-          answer = patternMap[el].count;
-        }
-      } else {
-        patternMap[el] = {
-          zeroCount: count,
-          count: -1,
-        };
-      }
+const lamps = IN.map((el) => el.trim());
+const maps = new Map();
+lamps.forEach((lamp) => {
+  if (maps.has(lamp)) {
+    maps.set(lamp, maps.get(lamp) + 1);
+  } else {
+    let result = 0;
+    lamp.split("").forEach((el) => {
+      if (el == "0") return ++result;
+      return 0;
+    });
+    if (result % 2 == K % 2 && result <= K) {
+      maps.set(lamp, 1);
     }
-  });
-};
-solve();
-console.log(answer);
+  }
+});
+console.log(maps.size == 0 ? 0 : Math.max(...maps.values())); // let answer = 0;
+
+// const find = (el) => {
+//   const count = el.split("").reduce((init, val) => {
+//     return init + !Number(val);
+//   }, 0);
+//   return count;
+// };
+// const solve = () => {
+//   pattern.map((el) => {
+//     if (patternMap[el] && patternMap[el].count != -1) {
+//       patternMap[el].count++;
+//       if (answer < patternMap[el].count) answer = patternMap[el].count;
+//     } else {
+//       const count = find(el);
+//       if (count % 2 == K % 2 && count <= K) {
+//         patternMap[el] = {
+//           zeroCount: count,
+//           count: 1,
+//         };
+//         if (answer < patternMap[el].count) {
+//           answer = patternMap[el].count;
+//         }
+//       } else {
+//         patternMap[el] = {
+//           zeroCount: count,
+//           count: -1,
+//         };
+//       }
+//     }
+//   });
+// };
+// solve();
+// console.log(answer);
