2d5de8416d5bb68888259b7c8427a64a7455fe99
문슬기 <tmfrlghtjr@gmail.com>
2023-03-18 01:26:24 +0900

    upload

--- 03.12 ~ 03.19 3월 두번째/A - 1946 신입 사원/슬기.py
+++ 03.12 ~ 03.19 3월 두번째/A - 1946 신입 사원/슬기.py
@@ -4,7 +4,20 @@ sys.stdin = open('input.txt')
 t = int(input())
 for _ in range(t):
     n = int(input())
+    scores = []
+
     for _ in range(n):
         grade, rank = map(int, input().split())
-        print(grade, rank)
-        print(n)
+        scores.append([grade, rank])
+    scores.sort(key=lambda x: (-x[0], x[1]))
+    print(scores)
+    cnt = 0
+    # g_min = scores[-1][0]
+    # r_min = scores[0][1]
+    for i in range(n):
+        for j in range(i, n):
+            if scores[i][0] > scores[j][0]:
+                if scores[i][1] < scores[j][1]:
+                    cnt += 1
+                    break
+    print(cnt)
