9634483039f9bd51ba59e5990d8847998e94eca5
문슬기 <tmfrlghtjr@gmail.com>
2023-03-23 00:25:00 +0900

    upload

--- 03.19 ~ 03.26 초심 잡고 열심히/A - 2529 부등호/슬기.py
+++ 03.19 ~ 03.26 초심 잡고 열심히/A - 2529 부등호/슬기.py
@@ -3,26 +3,33 @@ sys.stdin = open('input.txt')
 
 k = int(input())
 inequality = input().split(' ')
-num = list(range(0, 10))
-# print(num)
-# print(inequality)
-small = num[:k+1]
-big = num[:len(num)-(k+2):-1]
-# print(big)
-result = []
-tmp = 0
-minn = min(small)
-maxx = max(small)
-for i in inequality:
-    for j in range(k):
-        if i == '<':
-            if small[j] <= tmp:
-                continue
-        else:
-            if small[j] > small[j+1]:
-                continue
-            else:
-                small[j], small[j+1] = small[j+1], small[j]
-                print(small)
 
-print(small)
+visited = [0] * 10
+max_ans = ''
+min_ans = ''
+
+def check(x, y, z):     # 왼쪽 숫자, 오른쪽 숫자, 부등호
+    if z == '<':
+        return x < y
+    else:
+        return x > y
+
+def solve(depth, s):
+    global max_ans, min_ans
+
+    if depth == k+1:
+        if len(min_ans) == 0:
+            min_ans = s
+        else:
+            max_ans = s
+        return
+
+    for i in range(10):
+        if visited[i] == 0:
+            if depth == 0 or check(s[-1], str(i), inequality[depth-1]):
+                visited[i] = 1
+                solve(depth+1, s+str(i))
+                visited[i] = 0
+solve(0, '')
+print(max_ans)
+print(min_ans)
