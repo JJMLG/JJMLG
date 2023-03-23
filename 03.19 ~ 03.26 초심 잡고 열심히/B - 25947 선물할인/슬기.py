24a6494f84dc7111c331a69dbeea2cad3b04d5e9
문슬기 <tmfrlghtjr@gmail.com>
2023-03-24 01:06:48 +0900

    upload seulgi

--- 03.19 ~ 03.26 초심 잡고 열심히/B - 25947 선물할인/슬기.py
+++ 03.19 ~ 03.26 초심 잡고 열심히/B - 25947 선물할인/슬기.py
@@ -4,22 +4,31 @@ sys.stdin = open('input.txt')
 n, b, a = map(int, input().split())
 present = list(map(int, input().split()))
 present.sort()
-
+visited = [0] * n
 sale = 0
 tmp = 0
 cnt = 0
 # idx = 0
 for i in range(n):
-    tmp += present[i]
-    if tmp <= b:
+    if (tmp + present[i]) <= b:
         tmp += present[i]
-        cnt += 1
+        cnt = i + 1
     else:
-        sale = 0
-        print(i, tmp)
-        for j in range(i):
-            print(j)
+        flag = 0
+        for j in range(i, -1, -1):
+            if visited[j]:
+                continue
+            if a == 0:
+                break
+            tmp -= present[j] // 2
+            a -= 1
+            visited[j] = 1
+            if (tmp + present[i]) <= b:
+                flag = 1
+                tmp += present[i]
+                cnt = i + 1
+                break
 
-        if sale == a:
+        if flag == 0:
             break
-    print(cnt)
+print(cnt)
