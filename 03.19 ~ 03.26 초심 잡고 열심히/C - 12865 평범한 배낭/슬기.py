9fee403f114aa3c4af17bebbac68fd0b5d815a09
문슬기 <tmfrlghtjr@gmail.com>
2023-03-26 17:14:03 +0900

    upload seulgi

--- 03.19 ~ 03.26 초심 잡고 열심히/C - 12865 평범한 배낭/슬기.py
+++ 03.19 ~ 03.26 초심 잡고 열심히/C - 12865 평범한 배낭/슬기.py
@@ -2,6 +2,20 @@ import sys
 sys.stdin = open('input.txt')
 
 n, k = map(int, input().split())
+
+stuff = [[0, 0]]
+arr = [[0] * (k+1) for _ in range(n+1)]
 for _ in range(n):
     w, v = map(int, input().split())
-    # print(w, v)
+    stuff.append([w, v])
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        w = stuff[i][0]
+        v = stuff[i][1]
+
+        if j < w:
+            arr[i][j] = arr[i-1][j]
+        else:
+            arr[i][j] = max(arr[i-1][j], v + arr[i-1][j-w])
+print(arr[n][k])
