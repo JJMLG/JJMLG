60baae9c30bc640e82f48840667787beb70e2eff
hyejin <87624430+hy2jin@users.noreply.github.com>
2023-04-05 01:00:05 +0900

    개인정보 수집 유효기간

---
+++ 04.03 ~ 04.09 프로그래머스 TWO/B - 개인정보 수집 유효기간/혜진.js
@@ -0,0 +1,22 @@
+const makeDateNum = (date) => {
+  date = date.split('.').map(v => parseInt(v))
+  return date[2] + (date[1] * 28) + (date[0] * 12 * 28)
+}
+
+function solution(today, terms, privacies) {
+  today = makeDateNum(today)
+  
+  const month = new Object()
+  terms.forEach(t => {
+    t = t.split(' ')
+    month[t[0]] = parseInt(t[1]) * 28
+  });
+
+  const ans = []
+  for (let i = 0; i < privacies.length; i++) {
+    const tmp = privacies[i].split(' ')
+    const date = makeDateNum(tmp[0]) + month[tmp[1]]
+    if (today >= date) ans.push(i + 1)
+  }
+  return ans
+}
