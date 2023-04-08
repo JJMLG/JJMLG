b5d9282f114ea401b5af65dc07b50245fb2ec3d8
JW_SURFACEPRO8 <sorrow4468@gmail.com>
2023-04-08 22:28:07 +0900

    정원 업데이트 A - 다음에 올 숫자

--- 04.03 ~ 04.09 프로그래머스 TWO/A - 다음에 올 숫자/정원.py
+++ 04.03 ~ 04.09 프로그래머스 TWO/A - 다음에 올 숫자/정원.py
@@ -1,10 +1,7 @@
 def solution(common):
     c = common
-    answer = c[-1]
     
     if c[1]-c[0] == c[2]-c[1]: # 등차
-        answer += (c[1]-c[0])
+        return c[-1] + (c[1]-c[0])
     else: # 등비
-        answer *= (c[1]//c[0])
-    
-    return answer
+        return c[-1] * (c[1]//c[0])
