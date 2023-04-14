2164cb072a0831859cedd5dde8234b064b52f9c8
daedaem <tg8685@gmail.com>
2023-04-15 00:22:55 +0900

    Add 04.10 ~ 04.17 2023 KAKAO BLIND RECRUITMENT(해성)

--- 04.10 ~ 04.17 2023 KAKAO BLIND RECRUITMENT/C - 이모티콘 할인행사/해성.js
+++ 04.10 ~ 04.17 2023 KAKAO BLIND RECRUITMENT/C - 이모티콘 할인행사/해성.js
@@ -1,36 +1,38 @@
 function solution(users, emoticons) {
-    let result = [0,0];
-    
-    const ratios = new Array(emoticons.length).fill(0);
-    let calc = ()=>{
-        let signCnt=0;
-        let totalVal=0;
-        users.map(el=>{
-            let tempsum = 0;
-            for(let idx = 0; idx< ratios.length; idx++){
-                if(ratios[idx]>= el[0]){
-                    tempsum+= (emoticons[idx]* (100-ratios[idx])/100);
-                }else continue;
-            }
-            if(tempsum>=el[1]) {
-                signCnt++;
-            }else totalVal+=tempsum;
-        })
-        return [signCnt, totalVal];
+  let result = [0, 0];
+
+  const ratios = Array.from({ length: emoticons.length }, () => 0);
+  let calc = () => {
+    let signCnt = 0;
+    let totalVal = 0;
+
+    users.forEach((el) => {
+      let tempsum = 0;
+      for (let idx = 0; idx < ratios.length; idx++) {
+        if (ratios[idx] >= el[0]) {
+          tempsum += (emoticons[idx] * (100 - ratios[idx])) / 100;
+        } else continue;
+      }
+      if (tempsum >= el[1]) {
+        signCnt++;
+      } else totalVal += tempsum;
+    });
+    return [signCnt, totalVal];
+  };
+  const dfs = (dep) => {
+    if (dep === emoticons.length) {
+      const [signCnt, totalVal] = calc();
+      if (signCnt > result[0]) result = [signCnt, totalVal];
+      else if (signCnt === result[0] && totalVal > result[1])
+        result = [signCnt, totalVal];
+      return;
     }
-    const dfs=(dep)=>{
-        if(dep === emoticons.length){
-            const [signCnt, totalVal] = calc();
-            if(signCnt> result[0]) result = [signCnt, totalVal];
-            else if(signCnt===result[0] && totalVal> result[1]) result = [signCnt, totalVal];
-            return;
-        }
-        for(let val = 10; val<=40; val+=10){
-            ratios[dep]=val;
-            dfs(dep+1, val);
-            ratios[dep]=0;
-        }
+    for (let val = 10; val <= 40; val += 10) {
+      ratios[dep] = val;
+      dfs(dep + 1, val);
+      ratios[dep] = 0;
     }
-    dfs(0);
-    return result;
+  };
+  dfs(0);
+  return result;
 }
