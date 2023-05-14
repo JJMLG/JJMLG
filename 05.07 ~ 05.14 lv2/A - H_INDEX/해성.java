c4232218f44e999f80c67eaa3ffd151eb2898803
조해성 <tg8685@gmail.com>
2023-05-14 20:36:31 +0900

    docs: add 05.07 ~ 05.14 lv2

---
+++ 05.07 ~ 05.14 lv2/A - H_INDEX/해성.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ 
+public class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+ 
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+ 
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+ 
+        return answer;
+    }
+}
