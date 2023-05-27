87d77b648fed4ae76aaae8c2ce8d65b5c7a63daa
조해성 <tg8685@gmail.com>
2023-05-27 22:36:39 +0900

    docs: add 05.21 ~ 05.28 5월 4번째주 A.캐시

---
+++ 05.21 ~ 05.28 5월 4번째주/A - 캐시/해성.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        if(cacheSize==0) return cities.length*5;
+        LinkedList<String> caches = new LinkedList<>();
+        for(int i=0; i< cities.length; i++){
+            String findCities = cities[i].toUpperCase();
+            if(caches.contains(findCities)){
+                caches.remove(caches.indexOf(findCities));
+                caches.add(findCities);
+                answer++;
+            }else{
+                if(caches.size()>=cacheSize){
+                    caches.remove(0);
+                    caches.add(findCities);
+                    answer+=5;
+                }else{
+                    caches.add(findCities);
+                    answer+=5;
+                }
+            }
+        }
+        return answer;
+    }
+}
