8f49992e5a6c9c6216413d11520c0a515d10d348
문슬기 <tmfrlghtjr@gmail.com>
2023-07-11 23:32:30 +0900

    upload seulgi

---
+++ 07.09 ~ 07.16 7월 셋째주/A - 큰 수 만들기/슬기.py
@@ -0,0 +1,20 @@
+def solution(number, k):
+    answer = ''
+
+    #     순서는 지켜야 함
+    stack = []
+
+    #     arr = []
+    #     def recur(cur, start):
+    #         if cur == len(number) - k:
+    #             print(arr)
+    #             return
+
+    #         for i in range(start, len(number)):
+    #             arr.append(number[i])
+    #             recur(cur+1, i + 1)
+    #             arr.pop()
+
+    #     recur(0, 0)
+
+    return answer
