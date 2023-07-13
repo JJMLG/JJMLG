183dbd272d68e0d9ccb09f0e74cb1b86af672b00
hyejin <87624430+hy2jin@users.noreply.github.com>
2023-07-14 08:58:19 +0900

    C - 택배 상자

--- 07.09 ~ 07.16 7월 셋째주/C - 택배 상자/혜진.py
+++ 07.09 ~ 07.16 7월 셋째주/C - 택배 상자/혜진.py
@@ -1,9 +1,9 @@
 def solution(order):
     st = []
     o = 0
-    for i in range(1, len(order) + 1):
-        st.append(i)
-        while st and st[-1] == order[o]:
-            o += 1
-            st.pop()
-    return o
+    for i in range(1, len(order) + 1):      # i는 1부터 order의 길이까지
+        st.append(i)                        # 일단 넣기
+        while st and st[-1] == order[o]:    # 스택 마지막과 주문이 같으면
+            st.pop()      # pop하고
+            o += 1        # 다음 주문
+    return o              # 주문 어디까지
