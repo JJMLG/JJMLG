6a201444a186a9c23447d6aa871e98246574cda7
hyejin <87624430+hy2jin@users.noreply.github.com>
2023-07-14 13:39:15 +0900

    A - 큰 수 만들기

--- 07.09 ~ 07.16 7월 셋째주/A - 큰 수 만들기/혜진.py
+++ 07.09 ~ 07.16 7월 셋째주/A - 큰 수 만들기/혜진.py
@@ -19,13 +19,13 @@
 
 # 스택
 def solution(number, k):
-    ans = []
+    ans = []                # 결과
     for n in number:
-        while ans and ans[-1] < n and k > 0:
-            ans.pop()
-            k -= 1
-        ans.append(n)
+        while ans and ans[-1] < n and k > 0:    # ans가 있고
+            ans.pop()                           # 마지막 숫자가 지금 숫자(n)보다 작고
+            k -= 1                              # 아직 제외 가능하면
+        ans.append(n)       # n 넣기
     
-    if k:
-        ans = ans[:-k]
+    if k:                   # 더 빼야 하면
+        ans = ans[:-k]      # 그만큼 더 빼기
     return ''.join(ans)
