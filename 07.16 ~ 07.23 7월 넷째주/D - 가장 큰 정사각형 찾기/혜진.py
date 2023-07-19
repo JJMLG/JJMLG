00e823148ab494d3b257df0c3b32f3798bf64fa8
hyejin <87624430+hy2jin@users.noreply.github.com>
2023-07-20 08:51:49 +0900

    D - 가장 큰 정사각형 찾기

--- 07.16 ~ 07.23 7월 넷째주/D - 가장 큰 정사각형 찾기/혜진.py
+++ 07.16 ~ 07.23 7월 넷째주/D - 가장 큰 정사각형 찾기/혜진.py
@@ -1,12 +1,12 @@
 def solution(board):
     R, C = len(board), len(board[0])
     ans = 0
-    for r in range(1, R):
+    for r in range(1, R):          # 한 변이 2인 사각형부터 확인하려고 1부터 시작
         for c in range(1, C):
-            if board[r][c]:
+            if board[r][c]:        # 0이면 어차피 사각형 안됨
                 x = board[r - 1][c]
                 y = board[r][c - 1]
                 z = board[r - 1][c - 1]
                 board[r][c] = min(x, y, z) + 1
                 ans = max(board[r][c], ans)
-    return ans * ans
+    return ans * ans        # 한 변 길이 구했으니까 넓이 return
