72728ac822010c3c63ff89788dad0cc7e1f9004a
hyejin <87624430+hy2jin@users.noreply.github.com>
2023-07-26 08:51:46 +0900

    C - 점 찍기

--- 07.16 ~ 07.23 7월 넷째주/C - 점 찍기/혜진.py
+++ 07.16 ~ 07.23 7월 넷째주/C - 점 찍기/혜진.py
@@ -2,7 +2,7 @@ from math import sqrt
 
 def solution(k, d):
     ans = 0
-    for x in range(0, d + 1, k):
-        y = sqrt(d * d - x * x)
-        ans += y // k + 1
+    for x in range(0, d + 1, k):    # x좌표
+        y = sqrt(d * d - x * x)     # y좌표 최대 값
+        ans += y // k + 1           # x좌표일 때 가능한 y좌표의 수를 더해줌
     return ans
