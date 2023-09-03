bb9421c8b5e0f41aa12344fc94acd2e9259b94ae
문슬기 <tmfrlghtjr@gmail.com>
2023-09-03 22:16:51 +0900

    upload

--- 08.20 ~ 08.27 8월 넷째주/C - 10703 - 유성/슬기.py
+++ 08.20 ~ 08.27 8월 넷째주/C - 10703 - 유성/슬기.py
@@ -1,49 +1,35 @@
 import sys
 sys.stdin = open('input.txt')
 
-# r, s = map(int, input().split())
-#
-# before = [input() for _ in range(r)]
-# after = [['.'] * s for _ in range(r)]
-#
-# pos = 1 << 14
+r, s = map(int, input().split())
+
+before = [input() for _ in range(r)]
+after = [['.'] * s for _ in range(r)]
+
+pos = 1 << 14   # 유성이 최종적으로 움직여야하는 거리
 # print(pos)
 
-def restoreMeteorPicture(picture):
-    # 입력된 문자열을 리스트로 변환
-    picture = [list(row) for row in picture]
+for i in range(s):
+    h_meteor = 0    # 가장 높은 유성 행 좌표 (좌표가 높아야 땅과의 거리가 가깝다.)
+    h_ground = 9999     # 가장 낮은 땅 행 좌표 (좌표가 낮아야 유성과의 거리가 가깝다.)
+    flag = 0
+    for j in range(r):
+        if before[j][i] == 'X':
+            # print(before)
+            h_meteor = max(h_meteor, j)
+            flag = 1    # 유성이 있는 좌표를 만나면
+        elif before[j][i] == '#':
+            h_ground = min(h_ground, j)
+    if flag:    # 유성이 있는 좌표에서 pos 계산
+        pos = min(abs(h_meteor-h_ground)-1, pos)
+        # print(pos)
 
-    # 유성의 위치 찾기
-    meteor_pieces = []
-    for i in range(len(picture)):
-        for j in range(len(picture[i])):
-            if picture[i][j] == 'X':
-                meteor_pieces.append((i, j))
+for i in range(r):
+    for j in range(s):
+        if before[i][j] == 'X':
+            after[i+pos][j] = 'X'   # 유성을 최종 pos만큼 움직임
+        if before[i][j] == '#':
+            after[i][j] = '#'
 
-    # 유성 떨어뜨리기
-    while True:
-        new_meteor_pieces = []
-        for i, j in meteor_pieces:
-            if i + 1 < len(picture) and picture[i + 1][j] == '.':
-                new_meteor_pieces.append((i + 1, j))
-                picture[i][j] = '.'
-                picture[i + 1][j] = 'X'
-        if not new_meteor_pieces:
-            break
-        meteor_pieces = new_meteor_pieces
-
-    # 결과 출력
-    result = [''.join(row) for row in picture]
-    return result
-
-# 입력 받기
-n, m = map(int, input().split())
-picture = []
-for _ in range(n):
-    row = input()
-    picture.append(row)
-
-# 결과 출력
-restored_picture = restoreMeteorPicture(picture)
-for row in restored_picture:
-    print(row)
+for i in range(r):
+    print(''.join(after[i]))
---
+++ 08.20 ~ 08.27 8월 넷째주/D - 3048 개미/슬기.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('input.txt')
+
+n, m = map(int, input().split())
+ant1 = input()
+ant2 = input()
+t = int(input())
+
+print(ant2)
