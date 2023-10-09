1883d9c243ff54de160e712f5374975436b7aef6
조해성 <tg8685@gmail.com>
2023-10-09 15:11:02 +0900

    docs: add D - 프로그래머스 없었는데요 있었습니다

--- 10.8 ~ 10.15 10월 2째주/D - 프로그래머스_있었는데요 없었습니다/해성.sql
+++ 10.8 ~ 10.15 10월 2째주/D - 프로그래머스_있었는데요 없었습니다/해성.sql
@@ -1,3 +1,4 @@
+-- ORACLE 전통적 표기법
 SELECT A.animal_id,
        B.name
 FROM   animal_ins A,
@@ -5,3 +6,12 @@ FROM   animal_ins A,
 WHERE  A.animal_id = B.animal_id(+)
        AND A.datetime > B.datetime
 ORDER  BY A.datetime; 
+
+
+
+-- ANSI SQL 표기법
+SELECT A.ANIMAL_ID, B.NAME 
+FROM ANIMAL_INS A
+LEFT JOIN ANIMAL_OUTS B ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE A.DATETIME > B.DATETIME 
+ORDER BY A.DATETIME;
