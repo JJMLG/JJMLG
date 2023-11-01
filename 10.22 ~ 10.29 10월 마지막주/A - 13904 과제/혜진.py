bcc3984b3cddbad5fa92fd2b70b31d974a8d099c
hyejin <87624430+hy2jin@users.noreply.github.com>
2023-11-01 13:09:26 +0900

    A - 13904 과제

---
+++ 10.22 ~ 10.29 10월 마지막주/A - 13904 과제/혜진.py
@@ -0,0 +1,20 @@
+from heapq import heappush, heappop
+
+N = int(input())
+ans = maxday = 0
+work = [False] * (maxday + 1)
+
+Q = []
+for _ in range(N):
+    d, w = map(int, input().split())
+    Q.append([d, w])
+    maxday = max(maxday, d)
+Q.sort(key=lambda x:(-x[1], x[0]))
+
+for d, w in Q:
+    for i in range(d, 0, -1):
+        if work[i]: continue
+        work[i] = True
+        ans += w
+        break
+print(ans)
